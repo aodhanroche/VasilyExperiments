fc6c3d2b750b54777d450af442232db2e7fb0b43
Aodhan Roche <aodhan@griptape.ai>
2023-12-11 12:15:09 -0800

    Added a README, added remaining type-hints, Renamed

--- json_storage_system/app.py
+++ json_storage_system/app.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 
-def save_data(data, filename: str) -> None:
+def save_data(data: any, filename: str) -> None:
     try:
         with open(filename, "w") as file:
             json.dump(data, file)
@@ -10,7 +10,7 @@ def save_data(data, filename: str) -> None:
         logging.info("There was an error saving the data")
 
 
-def load_data(filename: str):
+def load_data(filename: str) -> any:
     try:
         with open(filename, "r") as file:
             loaded_data = json.load(file)
